1d342966bf0f1f25208393d8b7dc675693e7bcbd
Dutydudu <dut.dudu123@gmail.com>
2022-08-19 19:56:01 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -2,9 +2,19 @@
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
+
+//Console.log
 console.log("Ola Mundo");
 
 //Laco
-for (let i=1; i <= 10; i++) {
+for (let i=1; i <=10; i++) {
   console.log(i);
+}
+
+//Laco
+for (let i=1; i <=10; i++) {
+  if(i % 2 == 0) {
+    console.log(i);
+  }
+  
 }
