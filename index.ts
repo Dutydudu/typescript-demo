6aea23ecbc9acbd72976fb0113a16808d8cbc21c
Dutydudu <dut.dudu123@gmail.com>
2022-08-19 19:49:52 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+console.log("Ola Mundo");
+
+//Laco
+for (let i=1; i <= 10; i++) {
+  console.log(i);
+}
