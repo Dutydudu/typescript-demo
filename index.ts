731723c4d114fc28f4f9de9ee047feed8b08af36
Dutydudu <dut.dudu123@gmail.com>
2022-08-19 21:02:06 -0300

    teste tabata

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-let title: string = "MyApp";
+let title: string = "Tabata";
 document.getElementById('app.title').innerHTML = title;
+
+let ciclo: string = "Ciclo";
+document.getElementById('app.ciclo').innerHTML = ciclo;
+
+let temp: string = "Temporizador";
+document.getElementById('app.temp').innerHTML = temp;
 
 //Counter
 let counter = 0;
@@ -8,6 +14,11 @@ let counter = 0;
 let intervalId = setInterval(() => {
   counter++;
   document.getElementById('app.counter').innerHTML = counter.toString();
+}, 1000);
+
+let intervalIdTemporizador = setInterval(() => {
+  counter+1;
+  document.getElementById('app.counterTemp').innerHTML = counter.toString();
 }, 1000);
 
 
